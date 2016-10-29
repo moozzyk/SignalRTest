57e352e769a6547e086a9b23298064be4d4f8448
Pawel Kadluczka <moozzyk@gmail.com>
2016-10-29 13:16:53 -0700

    Initial commit

---
+++ SignalRTest/Startup.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRTest.Startup))]
+
+namespace SignalRTest
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            app.Map("/echo", map => map.RunSignalR<EchoConnection>());
+        }
+    }
+}
